b86b0e3dcfdbe91909a309e4ba5902763e5944de
sentilabs01 <sentilabs01@gmail.com>
2025-05-24 19:20:49 -0700

    UI/logo adjustments and remove GitHub logo

--- app/page.tsx
+++ app/page.tsx
@@ -27,11 +27,6 @@ export default function Home() {
               Automate Workflows
             </h1>
             <div className="flex flex-row flex-wrap items-center justify-start gap-8 w-full">
-              <a href="https://github.com/sentilabs01/splite" target="_blank" rel="noopener noreferrer">
-                <svg className="h-8 md:h-10 lg:h-12 w-auto object-contain" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                  <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.385.6.113.82-.26.82-.577 0-.285-.01-1.04-.015-2.04-3.338.726-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.756-1.333-1.756-1.09-.745.083-.73.083-.73 1.205.085 1.84 1.237 1.84 1.237 1.07 1.834 2.807 1.304 3.492.997.108-.775.418-1.305.76-1.606-2.665-.304-5.466-1.334-5.466-5.93 0-1.31.468-2.38 1.236-3.22-.124-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.3 1.23a11.5 11.5 0 0 1 3.003-.404c1.02.005 2.047.138 3.003.404 2.29-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.873.12 3.176.77.84 1.235 1.91 1.235 3.22 0 4.61-2.803 5.624-5.475 5.92.43.37.823 1.102.823 2.222 0 1.606-.015 2.898-.015 3.293 0 .32.216.694.825.576C20.565 21.796 24 17.297 24 12c0-6.63-5.37-12-12-12z"/>
-                </svg>
-              </a>
               <a href="https://www.make.com/" target="_blank" rel="noopener noreferrer">
                 <img src="https://automationalien.s3.us-east-1.amazonaws.com/make-logo-png.png" alt="Make Logo" className="h-8 md:h-10 lg:h-12 w-auto object-contain" />
               </a>
